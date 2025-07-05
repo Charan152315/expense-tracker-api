7c0c830fa148a6c59442c575de3f38aeaa54a79d
charan <chcharansri23@gmail.com>
2025-07-05 19:41:08 +0530

    fix: move alembic upgrade to entrypoint script

--- app/main.py
+++ app/main.py
@@ -3,22 +3,10 @@ from app.database import engine
 from app import models
 from app.models import Base
 import os
-from alembic.config import Config
-from alembic import command
 from app.routes import auth,expense_routes,user_routes,group_routes,run_migrations
 
 #Base.metadata.create_all(bind=engine)
 
-def run_migrations():
-    base_path = os.path.abspath(os.path.dirname(__file__))
-    alembic_ini = os.path.join(base_path, "alembic.ini")
-    alembic_folder = os.path.join(base_path, "alembic")
-
-    config = Config(alembic_ini)
-    config.set_main_option("script_location", alembic_folder)
-    command.upgrade(config, "head")
-
-run_migrations()
 
 app=FastAPI(title="Expense Tracker API", version="1.0.0")
 app.include_router(auth.router)
